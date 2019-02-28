f8ae7f99b7f261ad38b2140e856abd2d16914dde
gaoruiqing123 <2432167727@qq.com>
2019-02-28 12:30:45 +0800

    day011日考

--- day0011/one.py
+++ day0011/one.py
@@ -2,7 +2,7 @@
 
 
 for i in range(1, 10):
-    for j in range(i, 10):
-        print("%d*%d==%d" % (i, j, i*j))
+    for j in range(1, i+1):
+        print("%d*%d==%d   " % (i, j, i*j) , end='')
     print("\n")
 
